5c77e1dd00d5dc18ae7c46fdc1b600408be0b79d
ZiyaQureshi <ziyaqureshi1023@gmail.com>
2026-04-25 13:21:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showMessage() {
+  const message = document.getElementById("message");
+  message.textContent =
+    "This project uses HTTPS, input sanitization, performance optimization, and traffic monitoring.";
+}
 
+function submitInput() {
+  const input = document.getElementById("userInput").value;
+  const cleanInput = DOMPurify.sanitize(input);
+
+  const output = document.getElementById("output");
+
+  if (cleanInput.trim() === "") {
+    output.textContent = "Please enter a valid message.";
+  } else {
+    output.textContent = "Sanitized Output: " + cleanInput;
+  }
+
+  document.getElementById("userInput").value = "";
+}
